580493825b8be7a3d564b5b496b2e225c3e63a40
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-05 21:57:22 +0300

    mm

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,24 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument is provided
-if ARGV.empty?
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-    exit 1
-  end
-  
-  # Get the argument
-  input_string = ARGV[0]
-  
-  # Define the regular expression
-  regex = /School\z/
-  
-  # Match the regular expression
-  match_result = input_string.match(regex)
-  
-  # Print the result
-  if match_result
-    puts "#{match_result[0]}$"
-  else
-    puts "$"
-  end
-  
+puts ARGV[0].scan(/School/).join
