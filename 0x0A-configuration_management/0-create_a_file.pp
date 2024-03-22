37afc7581f44ccb7d8d0882371c8d309a8829694
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-22 17:53:36 +0300

    mm

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file { '/temp/school':
-    ensure => 'filr',
+# creating file
+
+file { '/temp/school' :
+    path => '/temp/school',
     mode => '0744'
     owner => 'www-data'
     group => 'www-data'
-    contains => "I love Puppet",
-}
+    contains => 'I love Puppet',
+    }
