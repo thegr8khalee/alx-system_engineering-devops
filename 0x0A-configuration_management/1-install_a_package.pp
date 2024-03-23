87b44c1fd3acff48b75e7f8ad352d42a5083290e
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-23 14:14:35 +0300

    mm

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
 # installin a flask
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
+}
