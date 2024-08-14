7ae3c19c1b036447598b5114bcda22d10dc4ca39
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-08-14 04:02:47 +0300

    mm

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and prints the titles of the first 10 hot posts
+Uses reddit API to get 10 hot posts
 """
 
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    returns the top ten posts for a given subreddit
-    """
-    user = {"User-Agent": "Lizzie"}
-    url = requests.get(
-        "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit), headers=user
-    ).json()
-    try:
-        for post in url.get("data").get("children"):
-            print(post.get("data").get("title"))
-    except Exception:
+    """Get 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"user-agent": "request"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    data = response.json().get("data").get("children")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
